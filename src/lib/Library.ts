bc2131e67019a7d2f37cd9b0d562d4330de5f22f
Lukáš Janáček <ljanacek@netvoice.cz>
2023-05-29 19:50:27 +0200

    Changes some library functions.

--- src/lib/Library.ts
+++ src/lib/Library.ts
@@ -1,24 +1,8 @@
-import {useEffect} from "react";
-import {useAppSelector} from "../app/hooks";
-import {RootState} from "../app/store";
-import {useNavigate} from "react-router-dom";
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 export const bearer = (): string => {
     return 'Bearer ' + localStorage.getItem("authToken")
-}
-
-export const preFlight = () => {
-    const isLoggedIn = useAppSelector((state: RootState) => state.login.authToken)
-    const isExpired = new Date(useAppSelector((state: RootState) => state.login.expiration)!) < new Date()
-    const navigate = useNavigate()
-
-    useEffect(() => {
-        if (isLoggedIn == null || isExpired) {
-            navigate("/logout")
-        }
-    }, [])
 }
 
 export const fetchData = async (url: string, method: string, contentType: string, requestBody: object | null = null) => {
@@ -50,4 +34,39 @@ export const sendData = async (url: string, method: string, contentType: string 
     }
 
     return fetch(url, options)
+}
+
+export const initials = (username: string): string => {
+    const sarr = username.split(RegExp("\_|\-\\s"))
+    let initials = sarr[0].charAt(0)
+    if (sarr.length > 1) {
+        initials += sarr[1].charAt(0)
+    }
+    return initials
+}
+
+export const lpad = (what: string, length: number, withWhat: string = " "): string => {
+    let str = what
+    while (str.length < length) {
+        str = withWhat + str
+    }
+    return str
+}
+
+export const rpad = (what: string, length: number, withWhat: string = " "): string => {
+    let str = what
+    while (str.length < length) {
+        str += withWhat
+    }
+    return str
+}
+
+export const formatDate = (dateString: Date) => {
+    const date = new Date(dateString)
+    return date.getDate() + ". " + date.getMonth() + ". " + lpad(date.getHours().toString(), 2, "0") + ":" + lpad(date.getMinutes().toString(), 2, "0")
+}
+
+export enum Direction {
+    ASC = "ASC",
+    DESC = "DESC"
 }
