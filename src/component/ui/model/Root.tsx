8e637d085f27d7deb680813a5665ffc54d14854f
Lukáš Janáček <ljanacek@netvoice.cz>
2023-05-29 19:50:37 +0200

    Added main page.

--- src/component/ui/model/Root.tsx
+++ src/component/ui/model/Root.tsx
@@ -1,15 +1,66 @@
 import * as React from 'react';
-import {useAppSelector} from "../../../app/hooks";
-import {RootState} from "../../../app/store";
-import {redirect} from "react-router-dom";
+import {Button} from "@mui/material";
+import {Link} from "react-router-dom";
 
 export default function Root() {
-    const isLoggedIn = useAppSelector((state: RootState) => state.login.authToken)
-
-    if (isLoggedIn == null)
-        redirect("/login")
-
     return (
-        <></>
+        <>
+            <h1>Tournament manager</h1>
+            <p>A simple application to showcase and test the features of React.js 18 in conjunction with Spring Boot 3.</p>
+            <p>Main frontend page was built with Material UI (MUI for short).</p>
+            <p>Currently employed technologies include:</p>
+            <table style={{ border: '1px solid rgba(255, 85, 49, 0.36)', borderRadius: '4px', width: '65%', margin: '10px auto' }}>
+                <thead>
+                    <tr>
+                        <th>Frontend</th>
+                        <th>Backend</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Node.js 18.16</td>
+                        <td>Openjdk-19</td>
+                    </tr>
+                    <tr>
+                        <td>NPM 9.6.4</td>
+                        <td>Spring Boot 3.0.6</td>
+                    </tr>
+                    <tr>
+                        <td>Typescript 4.9.5</td>
+                        <td>Kotlin 1.8.21</td>
+                    </tr>
+                    <tr>
+                        <td>Vite 4.2.1</td>
+                        <td>Gradle 8.0</td>
+                    </tr>
+                    <tr>
+                        <td>React.js 18.2.0</td>
+                        <td>Postgres 42.5.4</td>
+                    </tr>
+                    <tr>
+                        <td>React Redux 8.0.5</td>
+                        <td>Flyway 9.16.0</td>
+                    </tr>
+                    <tr>
+                        <td>React Router 6.10</td>
+                        <td>Spring Security 3.0.6</td>
+                    </tr>
+                    <tr>
+                        <td>React Hook Form 7.43.0</td>
+                        <td>Spring Data 3.0.6</td>
+                    </tr>
+                </tbody>
+            </table>
+            <p>And more...</p>
+            <hr style={{ color: 'rgba(255, 85, 49, 0.36)', width: '65%' }}/>
+            <p>You can check out the source code at github.com:
+                <Button component={Link} to={'https://github.com/st58238/nnpia-sem01-frontend'} variant="outlined" className='btnCustom' sx={{ marginLeft: '10px' }}>
+                    Frontend
+                </Button>
+                <Button component={Link} to={'https://github.com/st58238/nnpia-sem01-backend'} variant="outlined" className='btnCustom' >
+                    Backend
+                </Button>
+            </p>
+        </>
     );
 }
